6d21010f531b6e4539b3fab069ed10d5e6bbe6d8
kulivers <kulivers@mail.ru>
2022-02-01 12:22:51 +0300

    init commit

---
+++ FrogTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FrogTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SequenceTask/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SequenceTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
